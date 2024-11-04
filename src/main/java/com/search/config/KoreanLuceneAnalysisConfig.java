50bc52a2655fb939da0210c93728bd250eac64a7
성선규 <sgseong@itembay.com>
2024-11-04 14:00:21 +0900

    테스트 성공

--- src/main/java/com/search/config/KoreanLuceneAnalysisConfig.java
+++ src/main/java/com/search/config/KoreanLuceneAnalysisConfig.java
@@ -1,7 +1,11 @@
 package com.search.config;
 
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
 import org.apache.lucene.analysis.ko.KoreanFilterFactory;
 import org.apache.lucene.analysis.ko.KoreanTokenizerFactory;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory;
+import org.apache.lucene.analysis.snowball.SnowballPorterFilterFactory;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
 import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurationContext;
 import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
 
@@ -9,16 +13,14 @@ public class KoreanLuceneAnalysisConfig  implements LuceneAnalysisConfigurer {
     @Override
     public void configure(LuceneAnalysisConfigurationContext context) {
         context.analyzer( "english" ).custom()
-                .tokenizer( "standard" )
-                .tokenFilter( "lowercase" )
-                .tokenFilter( "snowballPorter" )
+                .tokenizer( StandardTokenizerFactory.class)
+                .tokenFilter( LowerCaseFilterFactory.class)
+                .tokenFilter( SnowballPorterFilterFactory.class)
                 .param( "language", "English" )
-                .tokenFilter( "asciiFolding" );
+                .tokenFilter( ASCIIFoldingFilterFactory.class);
 
         context.analyzer("koreanAnalyzer").custom()
                 .tokenizer(KoreanTokenizerFactory.class)
-//                .tokenFilter(KoreanPartOfSpeechStopFilterFactory.class)
-//                .tokenFilter(KoreanReadingFormFilterFactory.class)
                 .tokenFilter(KoreanFilterFactory.class);
 
     }
